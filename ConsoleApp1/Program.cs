0445783cb3219ace8c309e598dde1dfc34df1387
Demo-net <bmishra.net2021@gmail.com>
2024-01-01 00:08:35 +0530

    mnb

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,8 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ki");
-            Console.WriteLine("Home");
+            Console.WriteLine("kikj");
+            Console.WriteLine("Homejhko");
             Console.ReadLine();
         }
     }
