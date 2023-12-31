7189890ff782b23901a2d546f908ee4ae864208a
Demo-net <bmishra.net2021@gmail.com>
2023-12-31 18:01:55 +0530

    add changed

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("lala");
+            Console.WriteLine("Pala");
             Console.ReadLine();
         }
     }
